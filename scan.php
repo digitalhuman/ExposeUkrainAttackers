cb756f2f7a91c72caf9dab084e70d3ea4b7274e1
root <root@covidvirus.news>
2022-02-26 17:33:59 +0000

    scanner.log

--- scan.php
+++ scan.php
@@ -6,7 +6,6 @@ $ipsets = json_decode($json, true);
 
 function addInfo($ip, $country, $netname, $abusePhone, $abuseMail, $address, $orgPhone){
     file_put_contents("ip_country_info.csv","{$ip},{$country},{$netname},{$abusePhone},{$abuseMail},{$address},{$orgPhone}\r\n", FILE_APPEND);
-    echo "{$ip},{$country},{$netname},{$abusePhone},{$abuseMail},{$address},{$orgPhone}\r\n";
 }
 
 function ipExists($ip)
@@ -29,41 +28,41 @@ foreach($ipsets["files"] as $ip_set){
 
     $list = file($ip_set["url"]);
 
-    $csv = array_map('str_getcsv', $list);
+    $ext = substr($ip_set["url"],0, -3);
+    if($ext === "csv") {
+        $csv = array_map('str_getcsv', $list);
 
-    for($i = 0; $i<count($csv); $i+=2){
-        $record = $csv[$i];
-        if(isset($record[1]) && ValidateIP($record[1])){
-            try
-            {
-                if(!ipExists($record[1])) {
+        for ($i = 0; $i < count($csv); $i += 2) {
+            $record = $csv[$i];
+            if (isset($record[1]) && ValidateIP($record[1])) {
+                try {
+                    if (!ipExists($record[1])) {
 
-                    $info = LookupIP($record[1]);
-                    $country = getCountry($info);
-                    if (trim($country[0]) !== 'RU') {
+                        $info = LookupIP($record[1]);
+                        $country = getCountry($info);
+                        if (trim($country[0]) !== 'RU') {
 
-                        $orgPhone = getOrgPhone($info);
-                        $orgAddress = getAddressInfo($info);
+                            $orgPhone = getOrgPhone($info);
+                            $orgAddress = getAddressInfo($info);
 
-                        $abuseInfo = getAbuseInfo($info);
-                        $netName = getNetName($info);
+                            $abuseInfo = getAbuseInfo($info);
+                            $netName = getNetName($info);
 
-                        addInfo(
-                            $record[1],
-                            $country,
-                            $netName,
-                            $abuseInfo["AbusePhone"],
-                            $abuseInfo["AbuseMail"],
-                            $orgAddress,
-                            $orgPhone
-                        );
+                            addInfo(
+                                $record[1],
+                                $country,
+                                $netName,
+                                $abuseInfo["AbusePhone"],
+                                $abuseInfo["AbuseMail"],
+                                $orgAddress,
+                                $orgPhone
+                            );
+                        }
+                        sleep(rand(1, 4));
                     }
-                    sleep(rand(1,4));
+                } catch (\Exception $err) {
+                    //
                 }
-            }
-            catch(\Exception $err)
-            {
-                //
             }
         }
     }
