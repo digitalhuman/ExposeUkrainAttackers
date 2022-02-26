9e0b5fb63558eea8b68ffbdeb59c83613b78d90b
root <root@covidvirus.news>
2022-02-26 16:26:53 +0000

    Double check if IP exists, added humanizor to prevent block

--- scan.php
+++ scan.php
@@ -6,8 +6,22 @@ $ipsets = json_decode($json, true);
 
 function addInfo($ip, $country, $netname, $abusePhone, $abuseMail){
     file_put_contents("ip_country_info.csv","{$ip},{$country},{$netname},{$abusePhone},{$abuseMail}\r\n", FILE_APPEND);
+}
 
-    echo "{$ip},{$country},{$netname},{$abusePhone},{$abuseMail}\r\n";
+function ipExists($ip)
+{
+    $fp = fopen("ip_country_info.csv", "r");
+    if($fp){
+        while($line = fgetcsv($fp,"512")){
+            if($ip == $line[0]){
+                fclose($fp);
+                return true;
+            }
+        }
+
+        fclose($fp);
+    }
+    return false;
 }
 
 foreach($ipsets["files"] as $ip_set){
@@ -19,16 +33,27 @@ foreach($ipsets["files"] as $ip_set){
     for($i = 0; $i<count($csv); $i+=2){
         $record = $csv[$i];
         if(isset($record[1]) && ValidateIP($record[1])){
-            $info = LookupIP($record[1]);
-            $country = getCountry($info);
-            if(trim($country) !== 'RU') {
-                $abuseInfo = getAbuseInfo($info);
-                $netName = getNetName($info);
-                if (!empty($abuseInfo)) {
-                    addInfo($record[1], $country[0], $netName[0], $abuseInfo["AbusePhone"][0], $abuseInfo["AbuseMail"][0]);
-                } else {
-                    addInfo($record[1], $country[0], $netName[0], "n/a", "n/a");
+            try
+            {
+                if(!ipExists($record[1])) {
+
+                    $info = LookupIP($record[1]);
+                    $country = getCountry($info);
+                    if (trim($country[0]) !== 'RU') {
+                        $abuseInfo = getAbuseInfo($info);
+                        $netName = getNetName($info);
+                        if (!empty($abuseInfo)) {
+                            addInfo($record[1], $country[0], $netName[0], $abuseInfo["AbusePhone"][0], $abuseInfo["AbuseMail"][0]);
+                        } else {
+                            addInfo($record[1], $country[0], $netName[0], "n/a", "n/a");
+                        }
+                    }
+                    sleep(rand(1,3));
                 }
+            }
+            catch(\Exception $err)
+            {
+                //
             }
         }
     }
